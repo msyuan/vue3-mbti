98dbb980a350aff502fe4828b18a5d2f1d75b952
msyuan <645418157@qq.com>
2024-05-27 22:37:52 +0800

    fix: 路由修改为hash模式

--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,14 @@
  * @Author: laoyuan
  * @Date: 2024-05-26 16:47:47
  */
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/home.vue";
 const Result = () => import("../views/result.vue");
 const Text = () => import("../views/text.vue");
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.VITE_BASE_PATH),
-  // history: createWebHashHistory(),
+  // history: createWebHistory(import.meta.env.VITE_BASE_PATH),
+  history: createWebHashHistory(),
   routes: [
     {
       path: "/",
